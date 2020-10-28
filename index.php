2cc7d73edc19c03aac59474a5e783141d14af838
Lucas Zaia <lucas.zaia30@gmail.com>
2020-10-28 11:49:36 -0300

    design

--- index.php
+++ index.php
@@ -4,10 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="script/jquery-3.5.1.js"></script>
     <script src="script/script.js"></script>
 </head>
 <body>
-    <div></div>
+    <div class="container">
+        <div class="card">
+            <div class="form-group">
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"  placeholder="Digite um Texto: "> </textarea>
+            </div>
+        </div>
+        <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
 </body>
 </html>
