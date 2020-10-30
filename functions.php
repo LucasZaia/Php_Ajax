08f06690915c9a216aaddc007cf9dd19232bd956
Lucas Zaia <lucas.zaia30@gmail.com>
2020-10-30 11:03:22 -0300

    json

--- functions.php
+++ functions.php
@@ -1,11 +1,16 @@
 <?php
  
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: OPTIONS, GET, PUT, POST, DELETE, PATCH');
+
     require 'texto.php';
 
     $tex = new Texto();
 
+    $obj = file_get_contents("php://input");
+    $obj = json_decode($obj);
 
-    $tex->setNome('oi');
+    $tex->setNome($obj->name);
 
     $tex->TextCad();
         
--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
 function send() {
+    const val = document.getElementById('textArea').value;
 
+    const obj = {
+        name: val,
+    }
+
+
+    $.ajax({
+        url: "http://localhost/php_Ajax/functions.php",
+        type: "POST",
+        data: JSON.stringify(obj),
+        dataType: 'json',
+
+    });
 }
