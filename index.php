600585d8442e25f2e8041565e50df8e5dd5ffd97
Lucas Zaia <lucas.zaia30@gmail.com>
2020-10-30 10:42:51 -0300

    script

--- index.php
+++ index.php
@@ -7,14 +7,18 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="script/jquery-3.5.1.js"></script>
     <script src="script/script.js"></script>
+    
 </head>
 <body>
     <div class="container">
+    <form>
         <div class="card">
             <div class="form-group">
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"  placeholder="Digite um Texto: "> </textarea>
+                <textarea class="form-control" id="textArea" rows="2"  placeholder="Digite um Texto: "> </textarea>
             </div>
         </div>
+    <form>
+        <button type="button" onclick="send()">Enviar</button>
         <ul class="list-group">
             <li class="list-group-item"></li>
             <li class="list-group-item"></li>
